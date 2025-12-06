ba9e96c7598cd92c4b5a23fdac4ffed8c0915ad5
steven <steven.dehoog@gmail.com>
2025-12-05 21:01:52 -0600

    Solved day 4

--- Day 04/day_04.py
+++ Day 04/day_04.py
@@ -14,22 +14,59 @@ def timer_func(func):
     return wrap_func
 
 
+def neighbors8(point):
+    """
+    Given a point (x, y), return a set of the 8 surrounding (x, y) points.
+    """
+    x, y = point
+    return {
+        (x - 1, y - 1),
+        (x,     y - 1),
+        (x + 1, y - 1),
+        (x - 1, y),
+        (x + 1, y),
+        (x - 1, y + 1),
+        (x,     y + 1),
+        (x + 1, y + 1),
+    }
+
+
 @timer_func
 def day04(filepath, part2=False):
     with open(filepath) as fin:
         lines = [line.strip() for line in fin.readlines()]
 
+    paper_rolls = set()
+    for x, line in enumerate(lines):
+        for y, char in enumerate(line):
+            if char == '@':
+                paper_rolls.add((x, y))
 
+    if not part2:
+        a_count = 0
+        for roll in paper_rolls:
+            if len(neighbors8(roll).intersection(paper_rolls)) < 4:
+                a_count += 1
 
-    return 0
+        return a_count
+    else:
+        start_rolls = len(paper_rolls)
+        while True:
+            r2r = set()
+            for roll in paper_rolls:
+                if len(neighbors8(roll).intersection(paper_rolls)) < 4:
+                    r2r.add(roll)
+            if len(r2r) == 0:
+                return start_rolls - len(paper_rolls)
+            paper_rolls.difference_update(r2r)
 
 
 def main():
-    assert day04('test04') == 1
+    assert day04('test04') == 13
     print(f"Part 1: {day04('input04')}")
 
-    # assert day04('test04', True) == 1
-    # print(f"Part 2: {day04('input04', True)}")
+    assert day04('test04', True) == 43
+    print(f"Part 2: {day04('input04', True)}")
 
 
 if __name__ == '__main__':
